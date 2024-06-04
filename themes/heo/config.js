210f5f705f822b76936fcb6e6228ae329c0fa01d
jim00jim <149576331+jim00jim@users.noreply.github.com>
2024-06-04 12:17:19 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,11 +15,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_1: '分享知识',
+  HEO_HERO_TITLE_2: '与实用网址',
+  HEO_HERO_TITLE_3: '天青色',
+  HEO_HERO_TITLE_4: 'celeste',
+  HEO_HERO_TITLE_5: '欢迎来到我的博客',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
