2be8f0295bc062c4e9de880a38d6e6c9582cd928
jim00jim <149576331+jim00jim@users.noreply.github.com>
2025-07-31 11:05:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: 'jim',
-  HEO_HERO_TITLE_2: 'celeste',
+  HEO_HERO_TITLE_1: '藤井',
+  HEO_HERO_TITLE_2: '树',
   HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '',
   HEO_HERO_TITLE_5: '仅供娱乐',
