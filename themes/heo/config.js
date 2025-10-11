fc4dea19cc52f113f3f821dfa6293db09edf4088
jim00jim <149576331+jim00jim@users.noreply.github.com>
2025-10-11 23:17:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -61,58 +61,58 @@ const CONFIG = {
     {
       title_1: 'AfterEffect',
       img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/%E7%BE%8E%E5%9B%BD%E5%BE%80%E4%BA%8B.png',
-      color_1: '#989bf8',
+      color_1: '#ffffff',
       title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/%E6%83%85%E4%B9%A6.jpg',
       color_2: '#ffffff'
     },
     {
       title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/f81a43e004d5109f425dc877f7ffdc97369814ea004668bf9.JPG',
+      color_1: '#ffffff',
       title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/crop1744012608949.jpg',
+      color_2: '#ffffff'
     },
     {
       title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/Image_67443959709635.jpg',
       color_1: '#ffffff',
       title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/Image_1729421580376.jpg',
       color_2: '#ffffff'
     },
     {
       title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/IMG_20240604_201134.jpg',
+      color_1: '#ffffff',
       title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/81a9d8dbf428a21e8f85d5f693e5a331ac27d8c314818c866.jpg',
+      color_2: '#ffffff'
     },
     {
       title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/538edcac04b52f617088f288a3de80961c2d0dfdfe1b3f75e.jpg',
+      color_1: '#ffffff',
       title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/3eb3d178c3a65de879ff0ecbe359a38b3ce998283f1c51b4d.jpg',
+      color_2: '#ffffff'
     },
     {
       title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/Screenshot_20240325_140227.jpg',
+      color_1: '#ffffff',
       title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/QM_1797283116.jpg',
+      color_2: '#ffffff'
     },
     {
       title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
+      img_1: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/B3D2AA4ECD0082140AAFE195F1F32E06.jpg',
+      color_1: '#ffffff',
       title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      img_2: 'https://raw.githubusercontent.com/jim00jim/image/refs/heads/main/1758177145409.jpeg',
+      color_2: '#ffffff'
     }
   ],
 
